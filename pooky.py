b362b8008e7e6bf303dff693f7285c9645f412b8
Andrew Wang <andrew@umbrant.com>
2009-03-27 13:13:36 -0400

    Inlined the code, MASSIVE SPEEDUPS :D :D :D :D
    Also fixed a stupid bug with my jumps. Durr. rot13 and squares pass with flying colors now.
    
    Finally, also added support for "!stdinputtext" for taking input on the last line of a ook program.

--- pooky.py
+++ pooky.py
@@ -15,8 +15,8 @@ def parse(raw):
     a,b,c = "Ook.", "Ook?", "Ook!"
     elements = raw.split()
     raw_commands = [a+b, b+a, a+a, c+c, a+c, c+a, c+b, b+c]
-    real_commands = ["next", "prev", "inc", "dec", "read",
-                    "print", "j1", "j2"]
+    real_commands = ["n", "p", "i", "d", "r",
+                    "w", "(", ")"]
 
     translate = dict(zip(raw_commands, real_commands))
 
@@ -25,96 +25,86 @@ def parse(raw):
         yield translate[command]
 
 
-class Ook:
-    """Implements the Ook commands."""
-
-    def __init__(self, raw):
-        self.memory = [0]*30000
-        self.mp = 0
-        self.commands = [x for x in parse(raw)]
-        self.cp = 0
-        # Cached j location, lookup
-        self.j1 = {}
-        self.j2 = {}
-
-        # Populate the cache, make a pass through the input
-        s = []
-        for i, cmd in enumerate(self.commands):
-            cmd = self.commands[i]
-            if cmd is "j1":
-                s.append(i)
-            elif cmd is "j2":
-                j1 = s.pop()
-                self.j1[j1] = i
-
-        self.j2 = dict(zip(self.j1.values(), self.j1.keys()))
-
-    def next(self):
-        cmd = self.commands[self.cp]
-        Ook.__dict__["_"+cmd](self)
-        self.cp += 1
-
-    def _next(self):
-        self.mp += 1
-
-    def _prev(self):
-        self.mp -= 1
-
-    def _inc(self):
-        self.memory[self.mp] += 1
-        if self.memory[self.mp] > 255:
-            self.memory[self.mp] = 0
-
-    def _dec(self):
-        self.memory[self.mp] -= 1
-        if self.memory[self.mp] < 0:
-            self.memory[self.mp] = 255
-
-    def _read(self):
-        print "In:",
-        c = raw_input()
-        if c:
-            c = c[0]
-            self.memory[self.mp] = ord(c)
-
-    def _print(self):
-        sys.stdout.write(chr(self.memory[self.mp]))
-        #sys.stdout.flush()
-
-    def _j1(self):
-        if self.cp not in self.j1:
-            offset = self.commands[self.cp:].index("j2")
-            # Cache the j1 and j2 locations
-            self.j1[self.cp] = self.cp + offset
-            self.j2[self.cp+offset] = self.cp
-        if self.memory[self.mp] == 0:
-            # Skipping the loop, condition not met
-            self.j1[self.cp]
-
-    def _j2(self):
-        if self.memory[self.mp] != 0:
-            #temp = self.commands[:self.cp+1]
-            #temp.reverse()
-            #offset = temp.index("j1")
-            #self.cp -= offset
-
-            # Jump back to stored j1
-            self.cp = self.j2[self.cp]
-
-
 def main():
     if len(sys.argv) < 2:
         print "Usage: pooky.py <filename>"
         sys.exit(0)
 
-    o = Ook(open(sys.argv[1]).read())
+    # Initialize
+    memory = [0]*30000
+    mp = 0
+    raw = open(sys.argv[1], "r").readlines()
 
-    print o.commands[:117]
+    # Look for "stdin" as the last line
+    stdin = ""
+    if raw[-1][0] == "!":
+        stdin = raw[-1][1:]
+        raw = raw[:-1]
 
-    end = len(o.commands)
-    while o.cp < end:
-        o.next()
+    commands = [x for x in parse("".join(raw))]
+    cp = 0
+    # Cached j location, lookup
+    j1 = {}
+    j2 = {}
+
+    # Populate the cache, make a pass through the input
+    s = []
+    for i, cmd in enumerate(commands):
+        cmd = commands[i]
+        if cmd is "(":
+            s.append(i)
+        elif cmd is ")":
+            j = s.pop()
+            j1[j] = i
+
+    j2 = dict(zip(j1.values(), j1.keys()))
+
+    end = len(commands)
+    while cp < end:
+        cmd = commands[cp]
+
+        if cmd is "n":
+            mp += 1
+
+        elif cmd is "p":
+            mp -= 1
+
+        elif cmd is "i":
+            memory[mp] += 1
+            if memory[mp] > 255:
+                memory[mp] = 0
+
+        elif cmd is "d":
+            memory[mp] -= 1
+            if memory[mp] < 0:
+                memory[mp] = 255
+
+        elif cmd is "r":
+            if len(stdin):
+                c = stdin[0]
+                stdin = stdin[1:]
+                memory[mp] = ord(c)
+            else:
+                return
+
+        elif cmd is "w":
+            sys.stdout.write(chr(memory[mp]))
+            #sys.stdout.flush()
+
+        elif cmd is "(":
+            if memory[mp] == 0:
+                # Skipping the loop, condition not met
+                cp = j1[cp]
+
+        elif cmd is ")":
+            if memory[mp] != 0:
+                # Jump back to stored j1
+                cp = j2[cp]
+
+        # Increment command pointer
+        cp += 1
+
 
 
 if __name__ == "__main__":
-    cProfile.run("main()")
+    main()
