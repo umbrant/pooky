a1ea3a783872e9828b053daecb69b3f74b93b68d
Andrew Wang <andrew@umbrant.com>
2009-03-31 08:14:06 -0400

    Blargh l2spell

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from distutils.core import setup
 import py2exe
-setup(windows=[{"script":"main.py"}], options={"py2exe":{"includes":["sip"]}})
+setup(windows=[{"script":"pooky.py"}], options={"py2exe":{"includes":["sip"]}})
